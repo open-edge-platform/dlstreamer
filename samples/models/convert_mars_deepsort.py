e08c334f6a8be900bdea68c9afb8b08065ce0162
Huiling Bao <huiling.bao@intel.com>
2026-01-07 14:25:28 +0800

    update license

--- samples/models/convert_mars_deepsort.py
+++ samples/models/convert_mars_deepsort.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # ==============================================================================
-# Copyright (C) 2021-2025 Intel Corporation
+# Copyright (C) 2021-2026 Intel Corporation
 #
 # SPDX-License-Identifier: MIT
 # ==============================================================================
