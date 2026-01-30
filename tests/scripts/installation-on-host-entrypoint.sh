eb4751971ff2e6f97ea8624d47e53c69c83ca397
Huiling Bao <huiling.bao@intel.com>
2026-01-30 14:38:40 +0800

    more

--- tests/scripts/installation-on-host-entrypoint.sh
+++ tests/scripts/installation-on-host-entrypoint.sh
@@ -154,7 +154,7 @@ export LD_LIBRARY_PATH=/opt/intel/dlstreamer/gstreamer/lib:/opt/intel/dlstreamer
 export LIBVA_DRIVERS_PATH=/usr/lib/x86_64-linux-gnu/dri
 export GST_VA_ALL_DRIVERS=1
 export PATH=/python3venv/bin:/opt/intel/dlstreamer/gstreamer/bin:/opt/intel/dlstreamer/build/bin:$PATH
-export PYTHONPATH=/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:/home/dlstreamer/dlstreamer/python:/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:
+export PYTHONPATH=/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:/opt/intel/dlstreamer/python:/opt/intel/dlstreamer/gstreamer/lib/python3/dist-packages:
 export TERM=xterm
 
 # Display the values of the environment variables
