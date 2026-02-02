54bf20f4f0628512814792dd03893cb4340f85f4
Dawid Michalowski <dawid.michalowski@intel.com>
2026-02-02 10:15:01 +0100

    Fix typo

--- samples/download_public_models.sh
+++ samples/download_public_models.sh
@@ -1142,7 +1142,7 @@ if array_contains "deeplabv3" "${MODELS_TO_PROCESS[@]}" || array_contains "all" 
     cd "$MODELS_PATH"
     echo "Downloading and converting: ${MODEL_DIR}"
 
-    # Create remporary new Python virtual environment for omz tools
+    # Create temporary new Python virtual environment for omz tools
     deactivate 2>/dev/null || true
     $PYTHON_CREATE_VENV -m venv "$HOME/.virtualenvs/dlstreamer_openvino_dev" || handle_error $LINENO
     source "$HOME/.virtualenvs/dlstreamer_openvino_dev/bin/activate"
