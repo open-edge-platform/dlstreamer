59d7e47d5d7265783a1f606e5c97ad210ea6404e
ZiningLi <zining.li@intel.com>
2026-02-03 16:56:45 +0800

    remove HAVE_G3DLIDARPARSE checks

--- src/monolithic/gst/3d_registrator/register_3d_elements.cpp
+++ src/monolithic/gst/3d_registrator/register_3d_elements.cpp
@@ -11,10 +11,8 @@
 #include "g3d_radarprocess_meta.h"
 #include "gstradarprocess.h"
 
-#if defined(HAVE_G3DLIDARPARSE)
 #include "g3d_lidar_meta.h"
 #include "g3dlidarparse.h"
-#endif
 
 extern "C" {
 
@@ -22,18 +20,14 @@ static gboolean plugin_init(GstPlugin *plugin) {
     // Register element
     if (!gst_element_register(plugin, "g3dradarprocess", GST_RANK_NONE, GST_TYPE_RADAR_PROCESS))
         return FALSE;
-#if defined(HAVE_G3DLIDARPARSE)
     if (!gst_element_register(plugin, "g3dlidarparse", GST_RANK_NONE, GST_TYPE_G3D_LIDAR_PARSE))
         return FALSE;
-#endif
 
     // Register metadata
     gst_radar_process_meta_get_info();
     gst_radar_process_meta_api_get_type();
-#if defined(HAVE_G3DLIDARPARSE)
     lidar_meta_get_info();
     lidar_meta_api_get_type();
-#endif
 
     return TRUE;
 }
