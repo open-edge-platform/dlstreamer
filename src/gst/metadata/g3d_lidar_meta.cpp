93d5cfc0e635200a777ed5f58dadb9807d59f716
ZiningLi <zining.li@intel.com>
2026-02-03 11:06:36 +0800

    Keep consistent with the add_lidarparse branch.

--- src/gst/metadata/g3d_lidar_meta.cpp
+++ src/gst/metadata/g3d_lidar_meta.cpp
@@ -12,17 +12,8 @@
 GType lidar_meta_api_get_type(void) {
     static GType type = 0;
     static const gchar *tags[] = { "lidar", NULL };
-
-    GType existing = g_type_from_name("LidarMetaAPI");
-    if (existing) {
-        return existing;
-    }
-
     if (g_once_init_enter(&type)) {
-        GType _type = g_type_from_name("LidarMetaAPI");
-        if (!_type) {
-            _type = gst_meta_api_type_register("LidarMetaAPI", tags);
-        }
+        GType _type = gst_meta_api_type_register("LidarMetaAPI", tags);
         g_once_init_leave(&type, _type);
     }
     return type;
@@ -55,17 +46,14 @@ const GstMetaInfo *lidar_meta_get_info(void) {
     }
 
     if (g_once_init_enter(&meta_info)) {
-        const GstMetaInfo *mi = gst_meta_get_info("LidarMeta");
-        if (!mi) {
-            mi = gst_meta_register(
-                LIDAR_META_API_TYPE,
-                "LidarMeta",
-                sizeof(LidarMeta),
-                lidar_meta_init,
-                (GstMetaFreeFunction)lidar_meta_free,
-                (GstMetaTransformFunction)NULL
-            );
-        }
+         const GstMetaInfo *mi = gst_meta_register(
+            LIDAR_META_API_TYPE,
+            "LidarMeta",
+            sizeof(LidarMeta),
+            lidar_meta_init,  
+            (GstMetaFreeFunction)lidar_meta_free,
+            (GstMetaTransformFunction)NULL
+        );
         g_once_init_leave(&meta_info, mi);
     }
     return meta_info;
