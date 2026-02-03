b21707944bb35d27b3ee5226eb1d761797dfe201
ZiningLi <zining.li@intel.com>
2026-02-03 14:06:27 +0800

    Adjust the positions of radarprocess and lidarparse.

--- src/monolithic/gst/3d_registrator/register_3d_elements.cpp
+++ src/monolithic/gst/3d_registrator/register_3d_elements.cpp
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: MIT
  ******************************************************************************/
 
-
 #include "config.h"
 
 #include <gst/gst.h>
@@ -19,26 +18,27 @@
 
 extern "C" {
 
-
 static gboolean plugin_init(GstPlugin *plugin) {
+	// Register g3dradarprocess element
 	if (!gst_element_register(plugin, "g3dradarprocess", GST_RANK_NONE, GST_TYPE_RADAR_PROCESS))
         return FALSE;
 #if defined(HAVE_G3DLIDARPARSE)
 	if (!gst_element_register(plugin, "g3dlidarparse", GST_RANK_NONE, GST_TYPE_G3D_LIDAR_PARSE))
 		return FALSE;
 #endif
+
+	// Register metadata
 	gst_radar_process_meta_get_info();
     gst_radar_process_meta_api_get_type();
 #if defined(HAVE_G3DLIDARPARSE)
 	lidar_meta_get_info();
 	lidar_meta_api_get_type();
 #endif
+
     return TRUE;
 }
 
-
 GST_PLUGIN_DEFINE(GST_VERSION_MAJOR, GST_VERSION_MINOR, 3delements, "DL Streamer 3D Elements", plugin_init,
                   PLUGIN_VERSION, PLUGIN_LICENSE, PACKAGE_NAME, GST_PACKAGE_ORIGIN)
-
 
 } // extern "C"
