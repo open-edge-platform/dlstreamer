f1861b027aefb0513612be46de9c678bb8550c65
marcin-wadolkowski <marcin.wadolkowski@intel.com>
2026-02-04 10:53:03 +0100

    Fixes yolov8-seg inference result different from Openvino, removed unused code

--- src/monolithic/inference_backend/image_inference/openvino/model_api_converters.cpp
+++ src/monolithic/inference_backend/image_inference/openvino/model_api_converters.cpp
@@ -596,27 +596,18 @@ std::map<std::string, GstStructure *> get_model_info_preproc(const std::shared_p
             g_value_unset(&gvalue);
         }
         if (element.first == "reverse_input_channels") {
-            GValue gvalue = G_VALUE_INIT;
-            g_value_init(&gvalue, G_TYPE_INT);
-            g_value_set_int(&gvalue, gint(false));
-
             std::transform(element.second.as<std::string>().begin(), element.second.as<std::string>().end(),
                            element.second.as<std::string>().begin(), ::tolower);
 
             GValue color_value = G_VALUE_INIT;
             g_value_init(&color_value, G_TYPE_STRING);
             if (element.second.as<std::string>() == "yes" || element.second.as<std::string>() == "true") {
-                g_value_set_int(&gvalue, gint(true));
                 g_value_set_string(&color_value, "RGB");
             } else {
                 g_value_set_string(&color_value, "BGR");
             }
             gst_structure_set_value(s, "color_space", &color_value);
             g_value_unset(&color_value);
-
-            gst_structure_set_value(s, "reverse_input_channels", &gvalue);
-            GST_INFO("[get_model_info_preproc] reverse_input_channels: %s", element.second.as<std::string>().c_str());
-            g_value_unset(&gvalue);
         }
         if (element.first == "reshape") {
             std::vector<int> size_values = element.second.as<std::vector<int>>();
