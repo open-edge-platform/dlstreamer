22b8db405e922902e5fd1dcca1e996286de073b6
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2026-05-15 11:30:25 +0200

    Revert - change queue name check and remove mutex unlock.

--- src/monolithic/gst/inference_elements/base/inference_impl.cpp
+++ src/monolithic/gst/inference_elements/base/inference_impl.cpp
@@ -1139,7 +1139,7 @@ bool InferenceImpl::CheckSrcPadBlocked(GstObject *src) {
     if (dst == nullptr)
         return false;
 
-    if (g_str_has_prefix(dst->name, "queue")) {
+    if (strcmp(dst->name, "queue") > 0) {
         guint buf_cnt;
         g_object_get(dst, "current-level-buffers", &buf_cnt, NULL);
         GstState state, pending;
@@ -1376,10 +1376,6 @@ GstFlowReturn InferenceImpl::TransformFrameIp(GvaBaseInference *gva_base_inferen
         }
     }
 
-    // Release _mutex before SubmitImages.
-    lock.unlock();
-
-    return SubmitImages(gva_base_inference, metas, buffer);
 }
 
 void InferenceImpl::PushFramesIfInferenceFailed(
