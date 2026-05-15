419c7b5823c203df7e4b78d85175e70ef9bdbb44
nszczygl9 <118973656+nszczygl9@users.noreply.github.com>
2026-05-15 11:38:23 +0200

    add missing return

--- src/monolithic/gst/inference_elements/base/inference_impl.cpp
+++ src/monolithic/gst/inference_elements/base/inference_impl.cpp
@@ -1139,7 +1139,7 @@ bool InferenceImpl::CheckSrcPadBlocked(GstObject *src) {
     if (dst == nullptr)
         return false;
 
-    if (strcmp(dst->name, "queue") > 0) {
+    if (g_str_has_prefix(dst->name, "queue")) {
         guint buf_cnt;
         g_object_get(dst, "current-level-buffers", &buf_cnt, NULL);
         GstState state, pending;
@@ -1376,6 +1376,7 @@ GstFlowReturn InferenceImpl::TransformFrameIp(GvaBaseInference *gva_base_inferen
         }
     }
 
+    return SubmitImages(gva_base_inference, metas, buffer);
 }
 
 void InferenceImpl::PushFramesIfInferenceFailed(
