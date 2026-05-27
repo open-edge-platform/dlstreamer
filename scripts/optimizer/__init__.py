c3e53be4e8d53e0e175706a2774a64068b3c38ca
Blaszczak, Karol <karol.blaszczak@intel.com>
2026-05-27 12:10:05 +0200

    trim the repo to the docs content only

--- scripts/optimizer/__init__.py
+++
@@ -1,7 +0,0 @@
-# ==============================================================================
-# Copyright (C) 2025-2026 Intel Corporation
-#
-# SPDX-License-Identifier: MIT
-# ==============================================================================
-
-__all__ = ['DLSOptimizer', 'preprocess_pipeline'] # pylint: disable=undefined-all-variable
